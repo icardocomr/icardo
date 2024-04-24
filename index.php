8d8de406d3fca5f03d2e530c703cd8e5c6364d92
icardocomr <168036576+icardocomr@users.noreply.github.com>
2024-04-24 18:23:28 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="https://i.imgur.com/EnekpMa.png">
+    <title>Minhas Redes Sociais</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="container">
+        <img src="https://i.imgur.com/EnekpMa.png" alt="Logo" class="logo">
+        <h1>Minhas Redes Sociais</h1>
+        <div class="description">
+            <p>E aí, galera! Sou o Ricardo, o mais gato de todos! Tenho 2+1 anos de pura beleza e sou simplesmente o rei do Free Fire, com 94 FFWS no currículo (vão falar que é mentira, mas é pura verdade!). Sou nordestino raiz, pernambucano com muito orgulho! E aí, quem nasce em Pernambuco é o quê? É pernambucano, ué! Eita, cabra arretado!</p>
+        </div>
+        <ul>
+            <li>
+                <div class="social-media">
+                    <a href="https://www.tiktok.com/@icardocomr" target="_blank">
+                        <i class="fab fa-tiktok"></i>
+                    </a>
+                </div>
+            </li>
+            <li>
+                <div class="social-media">
+                    <a href="https://www.youtube.com/@icardocomr" target="_blank">
+                        <i class="fab fa-youtube"></i>
+                    </a>
+                </div>
+            </li>
+            <li>
+                <div class="social-media">
+                    <a href="https://www.instagram.com/p/C5V8KsXOSAk/" target="_blank">
+                        <i class="fab fa-instagram"></i>
+                    </a>
+                </div>
+            </li>
+            <li>
+                <div class="social-media">
+                    <a href="https://discord.gg/7a4z6bwWCx" target="_blank">
+                        <i class="fab fa-discord"></i>
+                    </a>
+                </div>
+            </li>
+            <li>
+                <div class="social-media">
+                    <a href="mailto:icardocomr@gmail.com">
+                        <i class="far fa-envelope"></i>
+                    </a>
+                </div>
+            </li>
+        </ul>
+    </div>
+</body>
+</html>
